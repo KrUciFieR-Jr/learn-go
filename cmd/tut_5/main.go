242d461964029fa3bc2d9ae5f54b8141979fb8cc
nair-j-karthik <nair.ka@northeastern.edu>
2024-08-12 20:32:11 -0700

    struct and interfaces

--- cmd/tut_5/main.go
+++ cmd/tut_5/main.go
@@ -3,12 +3,49 @@ package main
 import "fmt"
 
 type gasEngine struct {
-	mpg uint8
-	gallons uint8
+	mpg     uint16
+	gallons uint16
+	owner
 }
 
-func main(){
+type electricEngine struct {
+	mpkwh uint16
+	kwh   uint16
+	owner
+}
 
-	var myEngine gasEngine = gasEngine
+func (e gasEngine) milesLeft() uint16 {
+	return e.gallons * e.mpg
+}
+
+func (e electricEngine) milesLeft() uint16 {
+	return e.mpkwh * e.kwh
+}
+
+type owner struct {
+	name string
+}
+
+type engine interface {
+	milesLeft() uint16
+}
+
+func canMakeIt(eng engine, expectedMiles uint16) {
+	fmt.Println(eng.milesLeft())
+	if expectedMiles <= eng.milesLeft() {
+		fmt.Println("You can make it there")
+	} else {
+		fmt.Println("You can't make it there")
+	}
+}
+func main() {
+
+	var myGasEngine = gasEngine{25, 15, owner{"Karthik"}}
+	var myElectricEngine = electricEngine{15, 15, owner{"Karthik"}}
+	fmt.Println(myGasEngine.mpg, myGasEngine.gallons, myGasEngine.name)
+	fmt.Println(myElectricEngine.mpkwh, myElectricEngine.kwh, myElectricEngine.name)
+
+	canMakeIt(myElectricEngine, 250)
+	canMakeIt(myGasEngine, 250)
 
 }
